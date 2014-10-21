6b22c4fcfdd3561a88c82f25cd5adaf57692af9e
Iain R. Learmonth <irl@fsfe.org>
2014-10-21 13:08:50 +0100

    Add database class for web

--- web/database.py.ex
+++ web/database.py.ex
@@ -4,8 +4,8 @@ import mysql.connector
 class Database:
 
     def __init__(self):
-        self.cnx = mysql.connector.connect(user='root',
-                                      password='secret',
+        self.cnx = mysql.connector.connect(user='openqua',
+                                      password='xxxxxxxxxxxxxxx',
                                       host='127.0.0.1', database='openqua')
 
     def get_repeater(self, callsign):
@@ -26,6 +26,26 @@ class Database:
                      'lon': lon }
         cursor.close()
         return repeater
+
+    def get_all_repeaters(self):
+        cursor = self.cnx.cursor()
+        query = "SELECT * FROM repeater"
+        cursor.execute(query)
+        repeaters = []
+        for (callsign, rx, tx, to, mo, ml, lo, ke, lat, lon) in cursor:
+            repeater =  { 'callsign': callsign,
+                     'rx': rx,
+                     'tx': tx,
+                     'to': to,
+                     'mo': mo,
+                     'ml': ml,
+                     'lo': lo,
+                     'ke': ke,
+                     'lat': lat,
+                     'lon': lon }
+            repeaters.append(repeater)
+        return repeaters
+
 
     def close(self):
         self.cnx.close()
